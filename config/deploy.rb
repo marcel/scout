4d01ab8d4ed13aad0a0494681e15048c71ca30cf
Marcel Molina <marcel@twitter.com>
2013-10-12 15:57:27 -0700

    Enabled dir links.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :pty, true
 set :rvm_type, :user
 
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
