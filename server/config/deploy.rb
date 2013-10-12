cd2aa036e5fc21b74ad90c5639c66172e5ef9e44
Marcel Molina <marcel@twitter.com>
2013-10-12 14:24:28 -0700

    Update deploy config.

---
+++ server/config/deploy.rb
@@ -0,0 +1,42 @@
+require 'capistrano/rails'
+
+set :application, 'scout'
+set :user, 'scout'
+set :repo_url, 'git@github.com:marcel/scout.git'
+
+set :branch, 'master'
+set :deploy_to, "/home/scout/production"
+set :deploy_via, :remote_cache
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command.to_sym do
+      on roles(:all) do
+        run "service unicorn #{command}"
+      end
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+end
--- server/config/unicorn.rb
+++ server/config/unicorn.rb
@@ -1,10 +1,17 @@
-working_directory "/home/scout/server"
+root = "/home/scout/production/current"
+working_directory root
 listen 8080 
 worker_processes 10
-pid "/home/scout/server/tmp/pids/unicorn.pid"
+preload_app true
+timeout 240
+pid "#{root}/tmp/pids/unicorn.pid"
 user "scout", "scout"
-stderr_path "/home/scout/server/log/unicorn.log"
-stdout_path "/home/scout/server/log/unicorn.log"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
 
 before_fork do |server, worker|
 
