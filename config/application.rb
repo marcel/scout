98b376d5bed45e2f77935d6158449effc2f7de54
Marcel Molina <marcel@twitter.com>
2013-10-19 19:23:56 -0700

    Update the cache version now that accounts are all scoping cache keys

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ Bundler.require(:default, Rails.env)
 module Scout
   module Server
     class Application < Rails::Application
-      CACHE_VERSION = '1'
+      CACHE_VERSION = '2'
 
       # Settings in config/environments/* take precedence over those specified here.
       # Application configuration should go into files in config/initializers
