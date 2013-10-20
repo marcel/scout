ef9910abde17bee0c49accd5e572d4a96408c051
Marcel Molina <marcel@twitter.com>
2013-10-20 14:31:00 -0700

    Schedule imports with cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,17 +1,39 @@
 set :output, "#{path}/log/cron.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
 every :hour do
   runner "Game.update_forecasts"
 end
 
-# Learn more: http://github.com/javan/whenever
+every 2.hours do 
+  runner 'Team.import'
+  runner 'RosterSpot.import'
+end
+
+every 3.hours do
+  runner 'Player.import'
+end
+
+every :day, at: %w[13 19 23] do
+  runner 'Injury.import'
+  runner 'Projection.import'
+end
+
+every :friday, at: '6 am' do
+  runner 'PlayerPointTotal.import'
+end
+
+every :sunday, at: (17..23).to_a.map(&:to_s) do
+  runner 'PlayerPointTotal.import'
+end
+
+every :monday, at: %w[1am 2am 3am 4am 5am 6am] do
+  runner 'PlayerPointTotal.import'
+end
+
+every :tuesday, at: %[1am 2am 3am 4am 5am] do
+  runner 'PlayerPointTotal.import'
+end
+
+every [:thursday, :friday, :saturday], at: %w[13 19 23] do
+  runner 'ExpertRank.import'
+end
