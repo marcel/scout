5bb35879c91827ec0e546462709d83b9f510ef52
Marcel Molina <marcel@twitter.com>
2013-10-20 02:34:54 -0700

    Fix query in watches controller to explicitly reference players table now that it's loading watches, not players.

--- app/controllers/watch_controller.rb
+++ app/controllers/watch_controller.rb
@@ -3,11 +3,11 @@ class WatchController < ApplicationController
     query = current_account.watches.joins(:player).includes(:player)
 
     query = query.where("players.full_name LIKE ?", "%#{params[:name]}%") if params[:name]
-    query = query.where(position: params[:position].split(',')) if params[:position]
-    query = query.where(team_abbr: params[:team].split(',')) if params[:team]
-    query = query.where(position_type: params[:position_type].split(',')) if params[:position_type]
-    query = query.where(owner_key: params[:owner]) if params[:owner]
-    query = query.where(ownership_type: params[:ownership_type].split(',')) if params[:ownership_type]
+    query = query.where("players.position" => params[:position].split(',')) if params[:position]
+    query = query.where("players.team_abbr" => params[:team].split(',')) if params[:team]
+    query = query.where("players.position_type" => params[:position_type].split(',')) if params[:position_type]
+    query = query.where("players.owner_key" => params[:owner]) if params[:owner]
+    query = query.where("players.ownership_type" => params[:ownership_type].split(',')) if params[:ownership_type]
 
     @watches = query.load.sort_by do |watch|
       [-watch.votes, -watch.player.total_points]
